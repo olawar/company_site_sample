f53ada245a4cfd6f6b463dbcdcf5e37606c6d014
olawar <warzecha.ola@gmail.com>
2015-10-28 11:05:27 +0100

    Created new styling for team section and nav and added a function changing colors in the sticky menu.

--- js/app.js
+++ js/app.js
@@ -1,37 +1,62 @@
-$(function(){
+
 
 var Application = function(){
-        function init(){
-            console.log("init");
-        }
-        function scrollPage(){
-            console.log("scrolling");
-        }
-        return{
-            init:init,
-            scrollPage:scrollPage
-        }
-    };
+    function init(){
+        console.log("init");
+    }
+    function scrollPage(){
+        console.log("scrolling");
+    }
+    function scrollingFeatures(){
+
+        var menu = $("nav.sticky-nav");
+        var lastPositionTop = 0;
+        var sections = $("section");
+        var links = $(".activity");
+
+        $(window).on("scroll", function(){
+            if(menu.hasClass("sticky") === false && $(this).scrollTop() > menu.offset().top){
+                lastPositionTop = menu.offset().top;
+                menu.addClass("sticky");
+            }
+            if(menu.hasClass("sticky") && $(this).scrollTop() < lastPositionTop){
+                menu.removeClass("sticky");
+            }
+
+            sections.each(function(index){
+                if(index + 1 >= sections.length){
+                    if( sections.eq(index).offset().top < $(window).scrollTop()){
+                        links.eq(index).addClass("active");
+                    }
+                    else {
+                        links.eq(index).removeClass("active");
+                    }
+                }
+                else {
+                    if( sections.eq(index).offset().top < $(window).scrollTop() &&  sections.eq(index + 1).offset().top > $(window).scrollTop()){
+                        links.eq(index).addClass("active");
+                    }
+                    else {
+                        links.eq(index).removeClass("active");
+                    }
+                }
+            });
+        });
+    }
+
+    return{
+        init:init,
+        scrollPage:scrollPage,
+        scrollingFeatures:scrollingFeatures
+    }
+};
+
+
+$(function(){
 
     var app = new Application();
     app.init();
     app.scrollPage();
-
-
-    var menu = $("nav.sticky-nav");
-    var lastPositionTop = 0;
-    var menuHeight = menu.height();
-    var paragraphs = $("p");
-    var links = $(".nav a");
-
-    $(window).on("scroll", function(){
-        if(menu.hasClass("sticky") === false && $(this).scrollTop() > menu.offset().top){
-            lastPositionTop = menu.offset().top;
-            menu.addClass("sticky");
-        }
-        if(menu.hasClass("sticky") && $(this).scrollTop() < lastPositionTop){
-            menu.removeClass("sticky");
-        }
-    });
+    app.scrollingFeatures();
 
 });
